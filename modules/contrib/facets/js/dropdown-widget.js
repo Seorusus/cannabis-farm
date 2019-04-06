73f83cb28d486f955faedae7dd95a934cc0403cd
Oleg Kreminsky <seorusus@gmail.com>
2019-04-06 20:26:58 +0000

    Added fontawesome functional

--- modules/contrib/facets/js/dropdown-widget.js
+++ modules/contrib/facets/js/dropdown-widget.js
@@ -51,7 +51,7 @@
         .text(default_option_label);
       $dropdown.append($default_option);
 
-      $ul.prepend('<li class="default-option"><a href=".">' + default_option_label + '</a></li>');
+      $ul.prepend('<li class="default-option"><a href="">' + default_option_label + '</a></li>');
 
       var has_active = false;
       $links.each(function () {
