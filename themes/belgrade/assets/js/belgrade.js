01774a9fdf92c14d5cdde6a4334e90bb6e1eef9b
Oleg Kreminsky <seorusus@gmail.com>
2019-05-28 06:03:02 +0000

    Theming #19

--- themes/belgrade/assets/js/belgrade.js
+++ themes/belgrade/assets/js/belgrade.js
@@ -17,4 +17,22 @@
       }
     };
 
+    $(document).ready(function () {
+        var $menu = $("#menuF");
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 50 && $menu.hasClass("default")) {
+                $menu.fadeOut('fast', function () {
+                    $(this).removeClass("default")
+                        .addClass("fixed transbg")
+                        .fadeIn('fast');
+                });
+            } else if ($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+                $menu.fadeOut('fast', function () {
+                    $(this).removeClass("fixed transbg")
+                        .addClass("default")
+                        .fadeIn('fast');
+                });
+            }
+        });
+    });
   })(jQuery);
